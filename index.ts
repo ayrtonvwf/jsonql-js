82aa5750b84700ccbec288a8597c8adcd71297de
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2019-04-04 02:24:14 -0300

    First implementation attempt

---
+++ index.ts
@@ -0,0 +1,127 @@
+export enum Operation {
+    EqualsTo = "=",
+    LessThan = "<",
+    LessThanOrEqualTo = "<=",
+    GreaterThan = ">",
+    GreaterThanOrEqualTo = ">=",
+    DifferentThan = "!=",
+    MatchesRegex = "reg"
+}
+
+export enum ConditionsRelationship {
+    And = "and",
+    Or = "or",
+    Xor = "xor"
+}
+
+export declare type PropertyLocator = string[]
+
+export declare type DataObject = {
+    [key: string]: any
+}
+
+export declare type Data = DataObject[]
+
+export declare type Conditional = {
+    name: string,
+    operation: Operation,
+    value: PropertyLocator,
+    over: any
+}
+
+export declare type Conditions = {
+    relationship: ConditionsRelationship,
+    conditions: (string|Conditions)[]
+}
+
+export declare type Field = {
+    name: PropertyLocator,
+    value: PropertyLocator
+}
+
+export declare type Query = {
+    limit: number,
+    offset: number,
+    resultRoot: PropertyLocator,
+    root: PropertyLocator,
+    fields: Field[],
+    cond: Conditions,
+    named: Conditional[]
+}
+
+function resolveOperation(value: any, operation: Operation, over: any): boolean {
+    switch (operation) {
+        case Operation.EqualsTo:
+            return value === over;
+        case Operation.LessThan:
+            return value < over;
+        case Operation.LessThanOrEqualTo:
+            return value <= over;
+        case Operation.GreaterThan:
+            return value > over;
+        case Operation.GreaterThanOrEqualTo:
+            return value >= over;
+        case Operation.DifferentThan:
+            return value != over;
+        case Operation.MatchesRegex:
+            return RegExp(over).test(value)
+    }
+
+    return false;
+}
+
+function filterConditional(data: Data, conditional: Conditional): Data {
+    if (conditional.value.length === 1) {
+        return data.filter((value: any) =>
+            resolveOperation(value, conditional.operation, conditional.over)
+        )
+    }
+
+    const newConditional: Conditional = {
+        ...conditional
+    };
+    const currentProperty = conditional.value.shift();
+
+    if (currentProperty === undefined) {
+        throw new Error("No properties left to filter");
+    }
+
+    // @ts-ignore
+    data[currentProperty] = data[currentProperty].map(partialData =>
+        filterConditional(partialData, newConditional)
+    );
+
+    return data;
+}
+
+function resolveAndConditionals(data: Data, conditions: (string|Conditions)[], namedConditionals: Conditional[]): Data {
+    for (const condition of conditions) {
+        if (typeof condition === 'string') {
+            const conditional = namedConditionals.find(namedConditional => namedConditional.name === condition);
+            if (conditional === undefined) {
+                throw new Error("Unknown named conditional");
+            }
+            data = filterConditional(data, conditional)
+        } else {
+            data = resolveConditions(data, condition, namedConditionals)
+        }
+    }
+
+    return data
+}
+
+function resolveConditions(data: Data, conditions: Conditions, namedConditionals: Conditional[]): Data {
+    switch (conditions.relationship) {
+        case undefined:
+        case ConditionsRelationship.And:
+            return resolveAndConditionals(data, conditions.conditions, namedConditionals);
+        case ConditionsRelationship.Or:
+            return data;
+        case ConditionsRelationship.Xor:
+            return data;
+    }
+}
+
+export function query(data: Data, jsonql: Query): Data {
+    return resolveConditions(data, jsonql.cond, jsonql.named)
+}
